ec22f9d0b0e34664722b4383989cdc3d7661a2e9
tosh-m12 <tosh_m12@yahoo.co.jp>
2024-09-07 22:29:20 +0800

    課題mdn提出

---
+++ js/function.js
@@ -0,0 +1,14 @@
+const sayGoodMorning = () => {
+    console.log('おはようございます！');
+    console.log('昨日はよく眠れましたか？');
+    console.log('今日も一日頑張りましょう！');
+}
+
+const sayGoodEvening = () => {
+    console.log('こんばんは！');
+    console.log('今日も一日お疲れさまでした。');
+}
+
+sayGoodMorning();
+
+sayGoodEvening();
---
+++ js/object.js
@@ -0,0 +1,12 @@
+
+const personalData = { name: '侍太郎', age: 36, gender: '男性' };
+
+console.log(personalData);
+
+personalData.age = 37;
+
+personalData.address = '東京都';
+
+console.log(personalData);
+
+console.log(personalData.gender)
